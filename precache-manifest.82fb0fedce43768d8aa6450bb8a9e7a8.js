195cd1f0b4a7c554936d7a5f4fb28647befee113
unknown <inboxjedi@gmail.com>
2020-04-11 21:04:37 +0530

    Updates

---
+++ precache-manifest.82fb0fedce43768d8aa6450bb8a9e7a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e197a2c85aaf2063333b42552e75ed",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "252616e1e722505558c0",
+    "url": "/monster-rolodex/static/css/main.5db3ee41.chunk.css"
+  },
+  {
+    "revision": "a943139c20100a341bb3",
+    "url": "/monster-rolodex/static/js/2.1c933e26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.1c933e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252616e1e722505558c0",
+    "url": "/monster-rolodex/static/js/main.fda01b51.chunk.js"
+  },
+  {
+    "revision": "cb64d09352415fb132fd",
+    "url": "/monster-rolodex/static/js/runtime-main.2579c3f9.js"
+  }
+]);
